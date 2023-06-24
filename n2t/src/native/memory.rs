84f2b8c31f32a3aacd2ed34eb3606e30457c386f
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-06-23 20:36:46 -0500

    ram less silly and added test

--- n2t/src/native/memory.rs
+++ n2t/src/native/memory.rs
@@ -1,28 +1,23 @@
 #[derive(Clone, Debug, PartialEq)]
-pub struct RAM16K {
-    pub data: Vec<Vec<Vec<Vec<Vec<u16>>>>>,
+pub struct RAM {
+    pub data: Vec<u16>,
 }
 
-impl RAM16K {
+impl RAM {
     pub fn new() -> Self {
-        RAM16K {
-            data: vec![vec![vec![vec![vec![0u16; 8]; 8]; 8]; 8]; 4],
+        RAM {
+            data: vec![0; 16384],
         }
     }
 
-    pub fn cycle(&mut self, input: u16, address: u16, load: bool) -> u16 {
-        let l1 = ((address & 0b0011_0000_0000_1100) << 12) as usize;
-        let l2 = ((address & 0b0000_0000_0111_0000) << 9) as usize;
-        let l3 = ((address & 0b0000_0011_1000_0000) << 6) as usize;
-        let l4 = ((address & 0b0001_1100_0000_0000) << 3) as usize;
-        let l5 = (address & 0b1110_0000_0000_0000) as usize;
-
-        let out = &mut self.data[l1][l2][l3][l4][l5];
-
-        if load {
-            *out = input;
+    pub fn cycle(&mut self, input: u16, address: u16, load: u8) -> u16 {
+        if address > 0b0011_1111_1111_1111 {
+            panic!("address too high, got: {address:?}")
+        }
+        if load > 0 {
+            self.data[address as usize] = input;
         }
 
-        out.clone()
+        self.data[address as usize]
     }
 }
