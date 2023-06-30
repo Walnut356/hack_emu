f50f8574e68fdd635ad7b6e39e9a9f860d263b08
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-06-30 10:03:56 -0500

    fixed tests

--- n2t/tests/assembler_tests.rs
+++ n2t/tests/assembler_tests.rs
@@ -2,12 +2,13 @@ use n2t::software::assembler::*;
 use std::{
     fs::File,
     io::{BufReader, Read},
+    iter::zip,
     path::Path,
 };
 
 #[test]
 pub fn test_max() {
-    let path = Path::new(r#"src/software/asm files/Max.asm"#);
+    let path = Path::new(r#"../ch 6 asm files/Max.asm"#);
     let output = to_machine_code(&path);
 
     let out_file = File::open(output).unwrap();
@@ -21,12 +22,14 @@ pub fn test_max() {
     let mut test_stream = BufReader::new(test_against);
     test_stream.read_to_string(&mut buf2).unwrap();
 
-    assert_eq!(buf1, buf2);
+    for (line_test, line_target) in zip(buf1.lines(), buf2.lines()) {
+        assert_eq!(line_test, line_target);
+    }
 }
 
 #[test]
 pub fn test_pong() {
-    let path = Path::new(r#"src/software/asm files/Pong.asm"#);
+    let path = Path::new(r#"../ch 6 asm files/Pong.asm"#);
     let output = to_machine_code(&path);
 
     let out_file = File::open(output).unwrap();
@@ -40,5 +43,49 @@ pub fn test_pong() {
     let mut test_stream = BufReader::new(test_against);
     test_stream.read_to_string(&mut buf2).unwrap();
 
-    assert!(buf1 == buf2);
+    for (line_test, line_target) in zip(buf1.lines(), buf2.lines()) {
+        assert_eq!(line_test, line_target);
+    }
+}
+
+#[test]
+pub fn test_rect() {
+    let path = Path::new(r#"../ch 6 asm files/Rect.asm"#);
+    let output = to_machine_code(&path);
+
+    let out_file = File::open(output).unwrap();
+    let mut buf1 = String::new();
+    let mut out_stream = BufReader::new(out_file);
+    out_stream.read_to_string(&mut buf1).unwrap();
+
+    let test_path = Path::new(r#"../target_files/Rect_target.hack"#);
+    let test_against = File::open(test_path).unwrap();
+    let mut buf2 = String::new();
+    let mut test_stream = BufReader::new(test_against);
+    test_stream.read_to_string(&mut buf2).unwrap();
+
+    for (line_test, line_target) in zip(buf1.lines(), buf2.lines()) {
+        assert_eq!(line_test, line_target);
+    }
+}
+
+#[test]
+pub fn test_add() {
+    let path = Path::new(r#"../ch 6 asm files/Add.asm"#);
+    let output = to_machine_code(&path);
+
+    let out_file = File::open(output).unwrap();
+    let mut buf1 = String::new();
+    let mut out_stream = BufReader::new(out_file);
+    out_stream.read_to_string(&mut buf1).unwrap();
+
+    let test_path = Path::new(r#"../target_files/Add_target.hack"#);
+    let test_against = File::open(test_path).unwrap();
+    let mut buf2 = String::new();
+    let mut test_stream = BufReader::new(test_against);
+    test_stream.read_to_string(&mut buf2).unwrap();
+
+    for (line_test, line_target) in zip(buf1.lines(), buf2.lines()) {
+        assert_eq!(line_test, line_target);
+    }
 }
